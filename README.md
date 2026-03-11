ab359751def37552f427c21111f7bcfeb34a4abd
seanconnor03 <seanliam.connor@gmail.com>
2026-03-11 16:06:39 +0000

    Reorganised project structure and updated API key handling

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sean-Connor-road-risk-analysis-FYP
-Final year project, 2025-2026. 5 credit, 
--- run-project.ps1
+++
@@ -1,7 +0,0 @@
-cd risk-map\Backend
-start powershell {venv\Scripts\activate; python main.py}
-cd ..\
-cd Frontend 
-start powershell {npm run dev}
-Start-Process "http://localhost:5173/"
-
